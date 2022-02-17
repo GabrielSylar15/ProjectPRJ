ad7f33c6cb1bcfbdbff815273afe52a9cb6b8d0a
Sylar <neulaanh15@gmail.com>
2022-02-17 14:07:54 +0700

    fix bug nav bar

--- home.js
+++ home.js
@@ -1,28 +1,48 @@
 // Slide down animation
-var element = document.querySelector(".mobile__header .child");
-var heightElement = element.offsetHeight;
-element.classList.toggle("hide");
+// var element = document.querySelector(".mobile__header .child");
+// var heightElement = element.offsetHeight;
+// element.classList.toggle("hide");
+// var parentElement = document.querySelector(".mobile__nav");
+// var heightParentElement = parentElement.offsetHeight;
+// parentElement.classList.toggle("hide")
+// document.querySelector(".mobile__nav .parent>a").setAttribute('data-content',"+");
+// document.querySelector(".mobile__header .parent").onclick = function(){
+//     element.style.height = `${216}px`;
+//     element.classList.toggle("hide");
+//     parentElement.style.height = null;
+//     setTimeout(() => {
+//         if(element.offsetHeight!=0){
+//             document.querySelector(".mobile__nav .parent>a").setAttribute('data-content',"-");
+//             parentElement.style.height = parentElement.offsetHeight + "px";
+//         }else{
+//             document.querySelector(".mobile__nav .parent>a").setAttribute('data-content',"+");
+//             parentElement.style.height = parentElement.offsetHeight + "px";
+//         }
+//     }, 200);
+// }
+// parentElement.style.height = `${317}px`
+// document.querySelector(".header__bar").onclick = function(){
+//     parentElement.classList.toggle("hide")
+//     setTimeout(() => {
+//         console.log(parentElement.offsetHeight)
+//         if(parentElement.style.height<=parentElement.offsetHeight)   parentElement.style.height = `${parentElement.offsetHeight}px`;
+//     }, 250);
+// }
+
 var parentElement = document.querySelector(".mobile__nav");
-var heightParentElement = parentElement.offsetHeight;
-console.log(heightParentElement)
-parentElement.classList.toggle("hide")
+var element = document.querySelector(".mobile__header .child");
 document.querySelector(".mobile__nav .parent>a").setAttribute('data-content',"+");
 document.querySelector(".mobile__header .parent").onclick = function(){
-    element.style.height = `${heightElement}px`;
     element.classList.toggle("hide");
-    parentElement.style.height = null;
-    setTimeout(() => {
+        setTimeout(() => {
         if(element.offsetHeight!=0){
             document.querySelector(".mobile__nav .parent>a").setAttribute('data-content',"-");
-            parentElement.style.height = parentElement.offsetHeight + "px";
         }else{
             document.querySelector(".mobile__nav .parent>a").setAttribute('data-content',"+");
-            parentElement.style.height = parentElement.offsetHeight + "px";
         }
     }, 200);
 }
-parentElement.style.height = `${heightParentElement}px`
+
 document.querySelector(".header__bar").onclick = function(){
-    if(parentElement.offsetHeight!=0)   parentElement.style.height = `${parentElement.offsetHeight}px`;
     parentElement.classList.toggle("hide")
 }
