6734969e616fcbc76382bd2c4a4d98ee2b1f06a7
Sylar <neulaanh15@gmail.com>
2022-03-04 01:45:55 +0700

    fuking shit slide anmation, fkingshit bug

--- home.js
+++ home.js
@@ -29,6 +29,8 @@
 //     }, 250);
 // }
 
+
+// Slide down for Header
 var parentElement = document.querySelector(".mobile__nav");
 var element = document.querySelector(".mobile__header .child");
 document.querySelector(".mobile__nav .parent>a").setAttribute('data-content',"+");
@@ -64,8 +66,57 @@ function scrollFunctionP() {
   }else{
     document.querySelector(".header").classList.remove("scrollDownP");
   }
-  // console.log(document.documentElement.scrollTop)
 }
 
 
 // SlideShow Animation
+var high=0;
+var elementowl = document.querySelector(".owl");
+var owlitem = document.getElementsByClassName("owl-item");
+
+// document.getElementById("button--previous").onclick = function(){
+//   if(high<0){
+//     high+=400;
+//     elementowl.setAttribute("style",`transform: translateX(${high}px);transition: all .2s ease-in-out;`)
+//   }  
+  
+// }
+
+// document.getElementById("button--next").onclick = function(){
+//   if(high>-1600){
+//     high-=400;
+//     elementowl.setAttribute("style",`transform: translateX(${high}px);transition: all .2s ease-in-out;`)
+//   } 
+// }
+
+var screenwith = window.innerWidth;
+for (let index = 0; index < owlitem.length; index++) {
+  owlitem[index].setAttribute("style",`width:${screenwith/4}px`);
+  elementowl.setAttribute("style",`width: ${9*screenwith/4}px`)
+}
+
+document.querySelector("body").onresize = function(){
+  var screenwith = document.querySelector("body").offsetWidth;
+  console.log(screenwith)
+  if(window.innerWidth<740){
+    for (let index = 0; index < owlitem.length; index++) {
+      owlitem[index].setAttribute("style",`width:${screenwith/1}px`);
+      elementowl.setAttribute("style",`width: ${9*screenwith/1}px`)
+    }
+  }else if(window.innerWidth>=740 && window.innerWidth<1114){
+    for (let index = 0; index < owlitem.length; index++) {
+      owlitem[index].setAttribute("style",`width:${(screenwith)/3}px`);
+      elementowl.setAttribute("style",`width: ${9*screenwith/3}px`)
+    }
+  }else{
+    for (let index = 0; index < owlitem.length; index++) {
+      owlitem[index].setAttribute("style",`width:${screenwith/4}px`);
+      elementowl.setAttribute("style",`width: ${9*screenwith/4}px`)
+    }
+  }
+}
+
+elementowl.ondrag = function(){
+  console.log("hehe")
+}
+
